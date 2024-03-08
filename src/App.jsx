4a371b021d5cb8a06feb0d6d8c23b89f64ba4c80
André Silveira <andre.silveira1silva@gmail.com>
2024-03-08 15:18:41 -0300

    feat: add Post componet on structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header"
 import { Sidebar } from "./components/Sidebar"
+import { Post } from "./components/Post"
 
 import styles from './App.module.css'
 import "./global.css"
@@ -11,7 +12,10 @@ export function App() {
 
       <div className={styles.wrapper}>
         <Sidebar />
-        <main>main</main>
+        <main>
+          <Post />
+          <Post />
+        </main>
       </div>
     </div>
   )
