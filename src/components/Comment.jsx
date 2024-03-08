d1eecfca33ec52d41095083ca0fa5877a7bbe11b
André Silveira <andre.silveira1silva@gmail.com>
2024-03-08 15:16:48 -0300

    feat: add comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/andresilveira1.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>André Silveira</strong>
+              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30">Cerca de 1h atrás</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24}/>
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp size={20} weight="bold" />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
